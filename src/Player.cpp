7319d8c5f685050262dcea50c79dc739c9bf2724
Evan Casey <caseyem@appstate.edu>
2017-10-18 23:15:12 -0400

    Minor refactoring of Player class

--- src/Player.cpp
+++ src/Player.cpp
@@ -5,6 +5,25 @@
 #include "Dwarf.h"
 #include "Elf.h"
 
+
+Player::Player (std::string race, std::string name) {
+	set_name(name);
+	set_race(race);
+}
+
+std::string Player::get_name() {
+	return this->name;
+}
+void Player::set_name(std::string n) {
+	this->name = n;
+}
+std::string Player::get_race() {
+	return this->race;
+}
+void Player::set_race(std::string r) {
+	this->race = r;
+}
+
 void Player::move(int distance, std::string direction)
 {
 	//TODO
--- src/Player.h
+++ src/Player.h
@@ -3,25 +3,14 @@
 class Player 
 {
 	public:
-		Player (std::string race, std::string name) {
-			this->name = name;
-			this->race = race;
-		};
+		Player (std::string race, std::string name);
 		void move(int distance, std::string direction);
 		void show_equipped();
 		void show_inventory();
-		std::string get_name() {
-			return this->name;
-		}
-		void set_name(std::string n) {
-			this->name = n;
-		}
-		std::string get_race() {
-			return this->race;
-		}
-		void set_race(std::string r) {
-			this->race = r;
-		}
+		std::string get_name();
+		void set_name(std::string n);
+		std::string get_race();
+		void set_race(std::string r);
 		void show_attrs();
 
 	private:
