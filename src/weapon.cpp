33b695740ff690f30997259e4b4be151645cdc46
Evan Casey <caseyem@appstate.edu>
2017-11-15 18:24:32 -0500

    Tweaks to weapon damage/effects

--- src/weapon.cpp
+++ src/weapon.cpp
@@ -6,6 +6,8 @@
 Weapon::Weapon(std::string w_name, int attack_pow, int attack_pow_range) {
 	double condition_chance = ((double) rand() / RAND_MAX);
 	
+	this->is_poisonous = false;
+	this->poison_chance = 0.0;
 	if (this->LEGENDARY_CHANCE >= condition_chance) {
 		this->name = "Legendary " + w_name;
 		this->attack_power = (int)((double)attack_pow * this->LEGENDARY_MOD);
@@ -21,10 +23,22 @@ Weapon::Weapon(std::string w_name, int attack_pow, int attack_pow_range) {
 		this->attack_power = attack_pow;
 		this->attack_power_range = attack_pow_range;
 	}
-	if (w_name == "Sword" || w_name == "Dagger") {
-		this->bleed_effect = true;
-		if (w_name == "Dagger") {
-			this->poison_effect = true;
-		}
+	if (w_name == "Sword") {
+		this->bleed_chance = 0.15;
+		this->piercing_mod = 1.1;
+		this->slashing_mod = 1.3;
+		this->crushing_mod = 1.0;
+	}
+	else if (w_name == "Dagger") {
+		this->bleed_chance = 0.25;
+		this->piercing_mod = 1.4;
+		this->slashing_mod = 1.1;
+		this->crushing_mod = 1.0;
+	}
+	else {
+		this->bleed_chance = 0.05;
+		this->piercing_mod = 1.0;
+		this->slashing_mod = 1.0;
+		this->crushing_mod = 1.8;
 	}
 }
--- src/weapon.h
+++ src/weapon.h
@@ -6,8 +6,9 @@ class Weapon {
 		Weapon(std::string name, int attack_power, int attack_power_range);
 		int attack_power, attack_power_range;
 		std::string name;
-		bool bleed_effect;
-		bool poison_effect;
+		bool is_poisonous;
+		double bleed_chance, poison_chance;
+		double piercing_mod, slashing_mod, crushing_mod;
 
 	private:
 		const double ENHANCED_CHANCE  	  = 0.35;
