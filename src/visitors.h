77bfb8003073a919045cfd287385d180308fc579
Evan Casey <caseyem@appstate.edu>
2018-02-03 18:44:47 -0500

    Bring visitor styling more in line with rest of codebase

--- src/visitors.h
+++ src/visitors.h
@@ -13,16 +13,25 @@ class Visitors
 		{
 			WINDOW *win;
 			explicit output_desc(WINDOW *item_description_window) : win(item_description_window) {}
+			
 			template <typename T>
-			void operator()(T t) const { t.printType(win); t.printDescription(win); }
+			void operator()(T t) const 
+			{ 
+				t.printType(win); 
+				t.printDescription(win); 
+			}
 		};
 
 		struct output_inv_desc : public boost::static_visitor<>
 		{
 			WINDOW *win;
 			explicit output_inv_desc(WINDOW *item_description_window) : win(item_description_window) {}
+			
 			template <typename T>
-			void operator()(T t) const { t.printDescription(win); }
+			void operator()(T t) const 
+			{ 
+				t.printDescription(win); 
+			}
 		};
 
 		struct output_list_name : public boost::static_visitor<>
@@ -32,7 +41,10 @@ class Visitors
 			explicit output_list_name(int item_counter, WINDOW *inventory_window) : counter (item_counter), win(inventory_window) {}
 
 			template <typename T>
-			void operator()(T t) const { wprintw(win, "> [%d] %s\n", counter, (t.name).c_str()); }
+			void operator()(T t) const 
+			{ 
+				wprintw(win, "> [%d] %s\n", counter, (t.name).c_str()); 
+			}
 		};
 
 		struct output_name : public boost::static_visitor<>
@@ -41,13 +53,19 @@ class Visitors
 			explicit output_name(WINDOW *window) : win(window) {}
 
 			template <typename T>
-			void operator()(T t) const { wprintw(win, "%s\n", (t.name).c_str()); }
+			void operator()(T t) const 
+			{ 
+				wprintw(win, "%s\n", (t.name).c_str()); 
+			}
 		};
 
 		struct get_name : public boost::static_visitor<const char*>
 		{
 			template <typename T>
-			const char* operator()(T t) const { return (t.name).c_str(); }
+			const char* operator()(T t) const 
+			{
+				return (t.name).c_str(); 
+			}
 		};
 
 		struct pick_up : public boost::static_visitor<bool>
@@ -55,13 +73,19 @@ class Visitors
 			Player *p;
 			explicit pick_up(Player *player) : p(player) {}
 			template <typename T>
-			bool operator()(T t) const { return p->pickupLoot(t); }
+			bool operator()(T t) const 
+			{ 
+				return p->pickupLoot(t); 
+			}
 		};
 
 		struct get_item_id : public boost::static_visitor<int>
 		{
 			template <typename T>
-			int operator()(T t) const { return t.item_id; }
+			int operator()(T t) const 
+			{ 
+				return t.item_id; 
+			}
 		};
 };
 
