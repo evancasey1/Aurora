919b803edaf975684c1064febc75c99bebd12a26
Evan Casey <caseyem@appstate.edu>
2018-02-24 23:06:48 -0500

    Added comment to justify questionable function

--- src/food.h
+++ src/food.h
@@ -9,7 +9,8 @@ class Food : public Equipment
         Food(std::string name, int initial_health_gain, int health_gain_per_trigger, int turns_until_trigger, int total_triggers);
         void printDescription(WINDOW *win);
         void printType(WINDOW *win);
-        void compareTo(Food food, WINDOW *win){printDescription(win);};
+        void compareTo(Food food, WINDOW *win){printDescription(win);}; //This looks redundant, but is required to work smoothly with 
+                                                                        //the boost visitors. It's a necessary evil.
         
         std::string name;
         int initial_health_gain;     //Health boost right after eating
